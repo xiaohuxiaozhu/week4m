2e9f55920050dcdc261ee58f7d1ce5778b350cb0
xiaohuxiaozhu <polikon@hotmail.com>
2019-07-12 00:11:01 -0400

    first commit

---
+++ src/test/java/com/example/demo/HomeControllerTest.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HomeControllerTest {
+
+    @Test
+    public void homePage() {
+    }
+
+    @Test
+    public void blogSitePage() {
+    }
+}
